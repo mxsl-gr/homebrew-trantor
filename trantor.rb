2da52c45136d4c04bff0ab471babc86be451c9ae
mxsl-gr <mxsl.gr@gmail.com>
2020-02-28 16:56:47 +0800

    Update trantor.rb

--- trantor.rb
+++ trantor.rb
@@ -3,7 +3,7 @@ class Trantor < Formula
   homepage "https://www.terminus.io/"
   url "http://mxsl.oss-cn-hangzhou.aliyuncs.com/dist/trantor/trantor.0.0.1.tar.gz"
   version "0.0.1"
-  sha256 "94b08394258b8672e79b5deee1646a4b50315a3ab565d09720a3f75ea7be54aa"
+  sha256 "eb3ebac48ded490e070d72475116f1eb8fa67ea2909ebefce481fc5a26821b93"
 
   depends_on "docker"
 
